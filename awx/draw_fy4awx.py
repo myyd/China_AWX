486f2134457f7570ce0e764bf850451efb31a5fa
myyd <40823653+myyd@users.noreply.github.com>
2019-03-02 15:07:33 +0800

    Update draw_fy4awx.py

--- awx/draw_fy4awx.py
+++ awx/draw_fy4awx.py
@@ -12,7 +12,7 @@ import color
 #加载FY4A_AWX脚本
 import FY4A_AWX
 #颜色名称请进入color脚本中查看
-ys = color.nmc
+
 
 #交互式数据选择
 file = filedialog.askopenfilename(filetypes = [('AWX Files', '*.AWX')], title = '请选择要处理的风云4号AWX格式数据')
@@ -33,6 +33,19 @@ channel_number = awx.channel_number
 proj = ccrs.LambertConformal(central_longitude = lonmid, central_latitude = latmid, standard_parallels = [35])
 #设置自定义color与量程
 if channel_number in [1, 2, 3]:
+    colorbar = input('请选择颜色：1.nmc，2.bd，3.color，4.wv，5.ssdwv，6.bw。（不选择回车默认黑白色阶）')
+    if colorbar == '1':
+        ys = color.nmc
+    elif colorbar == '2':
+        ys = color.irbd
+    elif colorbar == '3':
+        ys = color.ircolor
+    elif colorbar == '4':
+        ys = color.wv
+    elif colorbar == '5':
+        ys = color.ssdwv
+    else:
+        ys = color.irbw
     my_cmap = mpl.colors.LinearSegmentedColormap('my_colormap', ys, 256)
     norm = mpl.colors.Normalize(-100, 50)
     linewidth1 = 0.5
